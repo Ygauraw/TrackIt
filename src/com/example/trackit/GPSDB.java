3367ff05883a92cf4f5f45597f992b9b64dfc9db
simplecode.snippet <simplecode.snippet@gmail.com>
2013-05-14 21:15:02 -0400

    Updated after proper code base structure.

--- src/com/example/trackit/GPSDB.java
+++ src/com/example/trackit/GPSDB.java
@@ -4,12 +4,16 @@ public class GPSDB {
 	public static final String DATABASE_NAME = "SATYA";
 	public static final int DATABASE_VERSION = 1;
 	public static final String TAG = "TRACKIT";
-	public static final int REQUEST_INTERVAL = 5000;
+	public static final int REQUEST_INTERVAL = 2000;
+
+	public static final String SERVER = "http://mpss.csce.uark.edu/~satya1";
+	public static final String SCRIPT_LOCATION = "/trackit/scripts/php/";
+	
+	public static final String LOGIN_URL = SERVER + SCRIPT_LOCATION + "trackit_login.php";
+	public static final String PUSH_URL = SERVER + SCRIPT_LOCATION +  "trackit_push.php";
+	public static final String POLL_URL = SERVER + SCRIPT_LOCATION + "trackit_poll.php";	
 
 	public static final String USER_TABLE = "route_";
-	public static final String PUSH_URL = "http://mpss.csce.uark.edu/~satya1/trackit_push.php";
-	public static final String POLL_URL = "http://mpss.csce.uark.edu/~satya1/trackit_poll.php";	
-
 	public static final String UID = "uid";
 	public static final String TIMESTAMP = "timestamp";
 	public static final String LAT = "latitude";
@@ -27,7 +31,6 @@ public class GPSDB {
 	public static final String SUCCESS = "success";
 	public static final String USERS = "users";
 	
-	public static final String LOGIN_URL = "http://mpss.csce.uark.edu/~satya1/trackit_login.php";
 	public static final String USERNAME = "username";
 	public static final String PASSWORD = "password";
 	public static final String FULLNAME = "userfullname";
